7542d02c638fd79f4d4bfd7d2f98ada96d840e13
William Xie <wxie@cs.utexas.edu>
2016-03-06 03:06:21 -0600

    Able to run submit batch job

--- finetune/finetune_subset.sh
+++ finetune/finetune_subset.sh
@@ -6,9 +6,12 @@ CAFFE_TOOL="${CAFFE_ROOT}/build/tools"
 CAFFE_DATA="${CAFFE_ROOT}/data/flickr_style"
 CAFFE_MODEL="${CAFFE_ROOT}/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel"
 
+TRAIN_VAL="/work/04018/wxie/maverick/visual_recognition/finetune/train_val_lmdb.prototxt"
+SOLVER="/work/04018/wxie/maverick/visual_recognition/finetune/solver_lmdb.prototxt"
+
     # finetune lmdb backend
-    ${CAFFE_TOOL}/caffe test -model train_val_lmdb.prototxt -weights ${CAFFE_MODEL} -gpu all
-    read -p $'Before finetuning.\nPress [Enter] key to continue.'
-    time ${CAFFE_TOOL}/caffe train -solver solver_lmdb.prototxt -weights ${CAFFE_MODEL} -gpu all
+    #${CAFFE_TOOL}/caffe test -model ${TRAIN_VAL} -weights ${CAFFE_MODEL} -gpu all
+    #read -p $'Before finetuning.\nPress [Enter] key to continue.'
+    time ${CAFFE_TOOL}/caffe train -solver ${SOLVER} -weights ${CAFFE_MODEL} -gpu all
     #read -p $'After finetuning.\nPress [Enter] key to continue.'
     #${CAFFE_TOOL}/caffe test -model train_val_lmdb.prototxt -weights finetune_flickr_style_lmdb_iter_1000.caffemodel -gpu all
