e2faa17548dbed9226ad8c6e8caa890b0f50b7f9
William Xie <wxie@cs.utexas.edu>
2016-03-08 15:18:04 -0600

    modified code to do long term finetuning

--- finetune/test_model.sh
+++ finetune/test_model.sh
@@ -6,8 +6,19 @@ CAFFE_TOOL="${CAFFE_ROOT}/build/tools"
 CAFFE_DATA="${CAFFE_ROOT}/data/flickr_style"
 CAFFE_MODEL="${CAFFE_ROOT}/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel"
 
-TRAIN_VAL="/work/04018/wxie/maverick/visual_recognition/finetune/train_val_lmdb.prototxt"
-SOLVER="/work/04018/wxie/maverick/visual_recognition/finetune/solver_lmdb.prototxt"
-WEIGHTS="/work/04018/wxie/maverick/visual_recognition/states/443733/finetune_lmdb_iter_600.caffemodel"
 
-    ${CAFFE_TOOL}/caffe test -model ${TRAIN_VAL} -weights ${WEIGHTS} -gpu all
+TRAIN="/work/04018/wxie/maverick/visual_recognition/finetune/test_train_lmdb.prototxt"
+TEST="/work/04018/wxie/maverick/visual_recognition/finetune/test_test_lmdb.prototxt"
+EXTRA="/work/04018/wxie/maverick/visual_recognition/finetune/test_extra_lmdb.prototxt"
+COMBINED="/work/04018/wxie/maverick/visual_recognition/finetune/test_combined_lmdb.prototxt"
+WEIGHTS="/work/04018/wxie/maverick/visual_recognition/states/finetune_lmdb_iter_54000.caffemodel"
+
+
+    ${CAFFE_TOOL}/caffe test -model ${TRAIN} -weights ${WEIGHTS} -gpu all
+    read -p $'TRAIN set done\nPress [Enter] key to continue.'
+    ${CAFFE_TOOL}/caffe test -model ${TEST} -weights ${WEIGHTS} -gpu all
+    read -p $'TEST set done\nPress [Enter] key to continue.'
+    ${CAFFE_TOOL}/caffe test -model ${EXTRA} -weights ${WEIGHTS} -gpu all
+    read -p $'EXTRA set done\nPress [Enter] key to continue.'
+    ${CAFFE_TOOL}/caffe test -model ${COMBINED} -weights ${WEIGHTS} -gpu all
+    read -p $'COMBINED set done\nPress [Enter] key to continue.'
